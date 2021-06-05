3f957e72d020409a1a6d1e88da90de85067e53b6
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-05 08:51:01 +0100

    Create String-repeat.js

---
+++ Javascript/8-Kyu/String-repeat.js
@@ -0,0 +1,10 @@
+// String repeat
+function repeatStr (n, s) {
+  
+    return s.repeat(n);
+
+};
+
+// Tests
+console.log(repeatStr(6, "I")); // "IIIIII"
+console.log(repeatStr(5, "Hello")); // "HelloHelloHelloHelloHello"
