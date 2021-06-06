db676a8fb92d6e27750407d22f474d7e821864ee
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-06 14:42:50 +0100

    Create Sum-the-strings.js

---
+++ Javascript/8-Kyu/Sum-the-strings.js
@@ -0,0 +1,10 @@
+// Sum the strings
+function sumStr(a,b) {
+  
+    return `${Number(a) + Number(b)}`;
+
+};
+
+// Tests
+console.log(sumStr('4', '5'));  // --> '9'
+console.log(sumStr('34', '5'));  // --> '39
