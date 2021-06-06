144a6aa7522bcc6d43022da5e7886ff402e2c7fc
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-06-06 14:41:54 +0100

    Create Return-strings.js

---
+++ Javascript/8-Kyu/Return-strings.js
@@ -0,0 +1,9 @@
+// Return strings
+function greet(name){
+    
+    return `Hello, ${name} how are you doing today?`;
+
+};
+
+// Test
+console.log(greet(Ryan));  // -->  'Hello, Ryan how are you doing today?'
