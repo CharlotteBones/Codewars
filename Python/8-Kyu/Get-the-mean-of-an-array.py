ea7762e2cfd1db03654b7266c251f4b447e4aef2
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-07 09:22:32 +0100

    Create Get-the-mean-of-an-array.py

---
+++ Python/8-Kyu/Get-the-mean-of-an-array.py
@@ -0,0 +1,10 @@
+# Get the mean of an array
+def get_average(marks):
+    
+    return sum(marks) // len(marks)
+
+# Tests
+print(get_average([2, 2, 2, 2]))  # --> 2
+print(get_average([1, 5, 87, 45, 8, 8]))  # --> 25
+print(get_average([2, 5, 13, 20, 16, 16, 10]))  # --> 11
+print(get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7]))  # --> 11
