e9792320d5dbd38ee971f67d9c7976ce3c5654aa
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-08 10:45:01 +0100

    Create Return-negative.py

---
+++ Python/8-Kyu/Return-negative.py
@@ -0,0 +1,9 @@
+# Return negative 
+def make_negative( number ):
+    
+    return -abs(number)
+
+# Tests
+print(make_negative(42))  # --> -42
+print(make_negative(-9))  # --> -9
+print(make_negative(0))  # --> 0
