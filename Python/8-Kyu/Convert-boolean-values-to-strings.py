713cd48cec2c1bc6985b1c4d2d0588ad9b78a059
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-12 16:43:25 +0100

    Create Convert-boolean-values-to-strings.py

---
+++ Python/8-Kyu/Convert-boolean-values-to-strings.py
@@ -0,0 +1,8 @@
+# Convert boolean values to strings 'Yes' or 'No'.
+def bool_to_word(boolean):
+    
+    return 'Yes' if boolean else 'No'
+
+# Tests
+print(bool_to_word(True))  # --> 'Yes'
+print(bool_to_word(False))  # --> 'No'
