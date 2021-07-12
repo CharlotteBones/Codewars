0744b0c30cb8040877176892cd4f790f828b055a
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-12 17:03:26 +0100

    Create Quarter-of-the-year.py

---
+++ Python/8-Kyu/Quarter-of-the-year.py
@@ -0,0 +1,9 @@
+# Quater of the year
+def quarter_of(month):
+    
+    return (month + 2) // 3
+
+# Tests
+print(quarter_of(3))  # --> 1
+print(quarter_of(8))  # --> 3
+print(quarter_of(11))  # --> 4
