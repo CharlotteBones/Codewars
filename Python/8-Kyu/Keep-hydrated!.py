2007b4970c0cf58e7efa45c0026627fce569ada6
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-13 07:54:04 +0100

    Create Keep-hydrated!.py

---
+++ Python/8-Kyu/Keep-hydrated!.py
@@ -0,0 +1,13 @@
+# Keep hydrated!
+def litres(time):
+
+    return time // 2
+
+# Tests
+print(litres(2))  # --> 1
+print(litres(1.4))  # --> 0
+print(litres(12.3))  # --> 6
+print(litres(0.82))  # --> 0
+print(litres(11.8))  # --> 5
+print(litres(1787))  # --> 893
+print(litres(0))  # --> 0
