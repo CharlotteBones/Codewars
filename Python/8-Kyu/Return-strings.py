2487d1a48976b3060a327589b8001a87dedca34d
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-14 13:59:33 +0100

    Create Return-strings.py

---
+++ Python/8-Kyu/Return-strings.py
@@ -0,0 +1,8 @@
+# Returning Strings
+def greet(name):
+    
+    return f'Hello, {name} how are you doing today?'
+
+#  Tests
+print(greet('Ryan'))  # --> "Hello, Ryan how are you doing today?"
+print(greet('Shingles'))  # --> "Hello, Shingles how are you doing today?"
