5410043f3c7b2e17aac935e488cc62bbd4f0d4fd
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-15 10:49:12 +0100

    Create Multiply.js

---
+++ Javascript/8-Kyu/Multiply.js
@@ -0,0 +1,12 @@
+// Multiply
+function multiply(a, b){
+  
+    return a * b
+ 
+ };
+
+// Tests
+console.log(multiply(1,1))  // --> 1
+console.log(multiply(2,1))  // --> 2
+console.log(multiply(2,2))  // --> 4
+console.log(multiply(3,5))  // --> 15 
