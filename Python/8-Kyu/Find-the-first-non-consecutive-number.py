7ee9ae8eb4bc701b650cbde53609238c64c2862c
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-15 12:21:48 +0100

    Create Find-the-first-non-consecutive-number.py

---
+++ Python/8-Kyu/Find-the-first-non-consecutive-number.py
@@ -0,0 +1,15 @@
+# Find the first non-consecutive number
+def first_non_consecutive(arr):
+    
+    for idx in range(0, len(arr) - 1):
+        if arr[idx] + 1 != arr[idx + 1] : return arr[idx + 1] 
+
+
+# Tests
+print(first_non_consecutive([1,2,3,4,6,7,8]))  # --> 6
+print(first_non_consecutive([1,2,3,4,5,6,7,8]))  # --> None
+print(first_non_consecutive([4,6,7,8,9,11]))  # --> 6
+print(first_non_consecutive([4,5,6,7,8,9,11]))  # --> 11
+print(first_non_consecutive([31,32]))  # --> None
+print(first_non_consecutive([-3,-2,0,1]))  # --> 0
+print(first_non_consecutive([-5,-4,-3,-1]))  # --> -1
