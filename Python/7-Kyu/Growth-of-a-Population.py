67c0ef23f1d991cf264360c88f5ac569eb5b783a
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-15 13:43:15 +0100

    Create Growth-of-a-Population.py

---
+++ Python/7-Kyu/Growth-of-a-Population.py
@@ -0,0 +1,15 @@
+# Growth of a population
+def nb_year(p0, percent, aug, p):
+    
+    years = 0
+
+    while p0 < p:
+        years += 1
+        p0 += (p0 * (percent / 100) // 1) + aug
+
+    return years
+
+# Tests
+print(nb_year(1500, 5, 100, 5000))  # --> 15
+print(nb_year(1500000, 2.5, 10000, 2000000))  # --> 10
+print(nb_year(1500000, 0.25, 1000, 2000000))  # --> 94
