ef28b1db18b1c22deb0649f0c6a067350f8d63e8
CharlotteBones <85293020+CharlotteBones@users.noreply.github.com>
2021-07-15 13:25:50 +0100

    Create String-repeat.py

---
+++ Python/8-Kyu/String-repeat.py
@@ -0,0 +1,9 @@
+# String repeat
+def repeat_str(repeat, string):
+
+    return repeat * string
+
+# Tests
+print(repeat_str(4, 'a'))  # --> 'aaaa'
+print(repeat_str(3, 'hello '))  # --> 'hello hello hello '
+print(repeat_str(2, 'abc'))  # --> 'abcabc'
